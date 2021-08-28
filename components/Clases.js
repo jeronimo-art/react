8630ad5a2de0f7f77713718be87e211e5846bba5
jeronimo-art <70788587+jeronimo-art@users.noreply.github.com>
2021-08-27 19:10:48 -0500

    React

---
+++ components/Clases.js
@@ -0,0 +1,17 @@
+import React from "react";
+/*
+import { BrowerRouter, Route, Switch, Link, NavLink } from "react-router-dom";
+import Contacto from "./components/Contacto";
+
+function Clases() {
+  return (
+    <Route>
+      <div className="Clases">
+        <Link to="/contacto">si</Link>
+      </div>
+    </Route>
+  );
+}
+
+export default Clases;
+*/
